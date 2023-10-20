02825c276d0c6297be7d7f78e09a896909b3cc1d
Levan <levan.soroznishvili@gmail.com>
2023-10-21 02:00:31 +0400

    Create UsersTableSeeder and edit UserFactory file

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111111'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+            [
+                'name' => 'Levan Soroznishvili',
+                'username' => 'leo',
+                'email' => 'leo@gmail.com',
+                'password' => Hash::make('111111'),
+                'role' => 'user',
+                'status' => 'active',
+            ],
+        ]);
+    }
+}
