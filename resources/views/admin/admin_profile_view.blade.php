eae7059edb4e4087c788fae5626302e1f86d1328
Levan <levan.soroznishvili@gmail.com>
2023-10-24 23:07:33 +0400

    Editable admin profile fields & image update

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.admin_dashboard')
 
-@section('title', 'Dashboard')
+@section('title', 'My Profile')
 
 @section('admin')
     <div class="content-header">
@@ -20,24 +20,25 @@
                     <div class="card card-primary card-outline">
                         <div class="card-body box-profile">
                             <div class="text-center">
-                                <img class="profile-user-img img-fluid img-circle"
-                                     src="{{ asset('backend/assets/dist/img/user4-128x128.jpg') }}"
-                                     alt="User profile picture">
+                                <img class="profile-user-img img-fluid img-circle" src="{{ (!empty($admin_data->photo)) ? url('upload/admin_images/'.$admin_data->photo) : url('upload/no_image.png') }}" alt="profile picture">
                             </div>
 
-                            <h3 class="profile-username text-center mb-3">John Doe</h3>
+                            <h3 class="profile-username text-center mb-3">{{ $admin_data->name }}</h3>
+                            <p class="text-muted text-center">@ {{ $admin_data->username }}</p>
 
                             <ul class="list-group list-group-unbordered">
                                 <li class="list-group-item">
-                                    <b>Full Name</b> <a class="float-right">John Doe</a>
+                                    <b>Full Name:</b> <a class="float-right">{{ $admin_data->name }}</a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>Phone:</b> <a class="float-right">{{ $admin_data->phone }}</a>
                                 </li>
                                 <li class="list-group-item border-bottom-0">
-                                    <b>E-Mail</b> <a class="float-right">john@gmail.com</a>
+                                    <b>E-Mail:</b> <a class="float-right">{{ $admin_data->email }}</a>
                                 </li>
                             </ul>
 
                         </div>
-                        <!-- /.card-body -->
                     </div>
                 </div>
                 <div class="col-md-9">
@@ -50,14 +51,38 @@
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="first_name">First Name</label>
-                                            <input type="text" name="first_name" id="first_name" class="form-control">
+                                            <label for="username">Username</label>
+                                            <input type="text" name="username" id="username" class="form-control" value="{{ $admin_data->username }}">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="last_name">Last Name</label>
-                                            <input type="text" name="last_name" id="last_name" class="form-control">
+                                            <label for="full_name">Full Name</label>
+                                            <input type="text" name="full_name" id="full_name" class="form-control" value="{{ $admin_data->name }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="email">E-Mail</label>
+                                            <input type="email" name="email" id="email" class="form-control" value="{{ $admin_data->email }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="phone">Phone</label>
+                                            <input type="tel" name="phone" id="phone" class="form-control" value="{{ $admin_data->phone }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12">
+                                        <img class="profile-user-img img-fluid img-square mb-2" id="show_image" src="{{ (!empty($admin_data->photo)) ? url('upload/admin_images/'.$admin_data->photo) : url('upload/no_image.png') }}" alt="profile picture">
+                                        <div class="form-group">
+                                            <label for="photo">Photo</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input" id="image" name="photo">
+                                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -72,4 +97,18 @@
         </div>
     </section>
 
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function() {
+           $('#image').change(function (e) {
+               var reader = new FileReader();
+               reader.onload = function (e) {
+                   $('#show_image').attr('src', e.target.result);
+               }
+               reader.readAsDataURL(e.target.files['0']);
+           })
+        });
+    </script>
 @endsection
