c554e9ee89d47a0080cec3237e9a4922440ac39b
Levan <levan.soroznishvili@gmail.com>
2023-10-30 23:53:41 +0400

    Post page design modification completed

--- resources/views/backend/news/add_news_post.blade.php
+++ resources/views/backend/news/add_news_post.blade.php
@@ -2,6 +2,20 @@
 
 @section('title', 'Add News Post')
 
+@section('styles')
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset('backend/assets/plugins/summernote/summernote-bs4.min.css') }}">
+
+    <!-- selectize -->
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/css/selectize.default.min.css"
+        integrity="sha512-pTaEn+6gF1IeWv3W1+7X7eM60TFu/agjgoHmYhAfLEU8Phuf6JKiiE8YmsNC0aCgQv4192s4Vai8YZ6VNM6vyQ=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+    />
+@endsection
+
 @section('admin')
     <div class="content-header">
         <div class="container-fluid">
@@ -24,9 +38,9 @@
                         <div class="card card-primary card-outline">
                             <div class="card-header py-3">
                                 <h3 class="card-title">Add News Post</h3>
+                                <button type="submit" class="btn btn-primary btn-xs btn-flat float-sm-right">Save</button>
                             </div>
                             <div class="card-body">
-
                                 <div class="form-group">
                                     <label for="news_title">News Title</label>
                                     <input type="text" class="form-control @error('news_title') is-invalid @enderror" name="news_title" id="news_title">
@@ -34,63 +48,10 @@
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
-
                                 <div class="form-group">
                                     <label for="news_details">News Details</label>
-                                    <textarea name="news_details" id="news_details" class="form-control @error('news_details') is-invalid @enderror" rows="5"></textarea>
-                                    @error('news_details')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
+                                    <textarea name="news_details" id="summernote">Place <em>some</em> <u>text</u> <strong>here</strong></textarea>
                                 </div>
-
-
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="name">Full Name</label>
-                                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name">
-                                            @error('name')
-                                            <span class="text-danger">{{ $message }}</span>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="username">Username</label>
-                                            <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username">
-                                            @error('username')
-                                            <span class="text-danger">{{ $message }}</span>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="email">E-Mail</label>
-                                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email">
-                                    @error('email')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="password">Password</label>
-                                    <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
-                                    @error('password')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="phone">Phone</label>
-                                    <input type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" id="phone">
-                                    @error('phone')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-
-                                <button type="submit" class="btn btn-primary">Save</button>
-
                             </div>
                         </div>
 
@@ -99,66 +60,85 @@
 
                         <div class="card card-primary card-outline">
                             <div class="card-header py-3">
-                                <h3 class="card-title">Category</h3>
+                                <h3 class="card-title">Post Details</h3>
                             </div>
                             <div class="card-body">
-                                <select name="category_id" class="custom-select">
-                                    <option selected>- Select Category -</option>
-                                    @foreach($categories as $category)
-                                        <option value="{{ $category->id }}">{{ $category->category_name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                            <div class="card-header py-3">
-                                <h3 class="card-title">Subcategory</h3>
-                            </div>
-                            <div class="card-body">
-                                <select name="subcategory_id" class="custom-select">
-                                    <option selected>- Select Subcategory -</option>
-                                    @foreach($subcategories as $subcategory)
-                                        <option value="{{ $subcategory->id }}">{{ $subcategory->subcategory_name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                            <div class="card-header py-3">
-                                <h3 class="card-title">Writer</h3>
-                            </div>
-                            <div class="card-body">
-                                <select name="user_id" class="custom-select">
-                                    <option selected>- Select Writer -</option>
-                                    @foreach($admin_user as $user)
-                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                            <div class="card-header py-3">
-                                <h3 class="card-title">News Post Photo</h3>
-                            </div>
-                            <div class="card-body">
-                                <img class="profile-user-img img-fluid img-square mb-2" id="show_image" src="{{ url('upload/no_image.png') }}" alt="">
                                 <div class="form-group">
-                                    <label for="show_image">Photo</label>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input type="file" class="custom-file-input" id="image" name="image">
-                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                    <label for="category_id">Select Category</label>
+                                    <select name="category_id" class="custom-select">
+                                        <option selected>- Select Category -</option>
+                                        @foreach($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="subcategory_id">Select Subcategory</label>
+                                    <select name="subcategory_id" class="custom-select">
+                                        <option selected>- Select Subcategory -</option>
+                                        @foreach($subcategories as $subcategory)
+                                            <option value="{{ $subcategory->id }}">{{ $subcategory->subcategory_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="user_id">Select Writer</label>
+                                    <select name="user_id" class="custom-select">
+                                        <option selected>- Select Writer -</option>
+                                        @foreach($admin_user as $user)
+                                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <img class="profile-user-img img-fluid img-square mb-2" id="show_image" src="{{ url('upload/no_image.png') }}" alt="">
+                                    <div class="form-group">
+                                        <label for="show_image">Select Photo</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" class="custom-file-input" id="image" name="image">
+                                                <label class="custom-file-label" for="image">Choose file</label>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+
+                                <div class="form-group">
+                                    <label for="view_section">View Section</label>
+                                    <div class="form-group mb-0">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="breaking_news" id="breaking_news" value="1">
+                                            <label class="form-check-label" for="breaking_news">Breaking News</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="top_slider" id="top_slider" value="1">
+                                            <label class="form-check-label" for="top_slider">Top Slider</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="first_section_three" id="first_section_three" value="1">
+                                            <label class="form-check-label" for="first_section_three">First Section Three</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="first_section_nine" id="first_section_nine" value="1">
+                                            <label class="form-check-label" for="first_section_nine">First Section Nine</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group mb-0">
+                                    <label for="tags">Select Tags</label>
+                                    <input type="text" class="selectize @error('selectize') is-invalid @enderror" name="tags" id="tags" value="awesome">
+                                </div>
+
                             </div>
                         </div>
 
                     </div>
                 </div>
+
             </form>
 
         </div>
@@ -178,4 +158,35 @@
             })
         });
     </script>
+
+    <!-- Selectize -->
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/js/selectize.min.js"
+        integrity="sha512-IOebNkvA/HZjMM7MxL0NYeLYEalloZ8ckak+NDtOViP7oiYzG5vn6WVXyrJDiJPhl4yRdmNAG49iuLmhkUdVsQ=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+    ></script>
+    <script>
+        $(".selectize").selectize({
+            delimiter: ",",
+            persist: false,
+            maxItems: null,
+            create: function (input) {
+                return {
+                    value: input,
+                    text: input,
+                };
+            }
+        });
+    </script>
+
+    <!-- Summernote -->
+    <script src="{{ asset('backend/assets/plugins/summernote/summernote-bs4.min.js') }}"></script>
+    <script>
+        $(function () {
+            // Summernote
+            $('#summernote').summernote();
+        });
+    </script>
+
 @endsection
