ae98c248bcfc594c05741f2a8a607c6f7aab980c
Levan <levan.soroznishvili@gmail.com>
2023-11-03 00:19:10 +0400

    Breaking news output completed

--- resources/views/frontend/body/breaking_news.blade.php
+++ resources/views/frontend/body/breaking_news.blade.php
@@ -1,22 +1,16 @@
+@php
+    $breaking_news = \App\Models\NewsPost::where('status', 1)->where('breaking_news', 1)->limit(7)->get();
+@endphp
+
 <div class="bg-light text-white text-center py-1 mb-3 border-bottom">
     <div class="container marquee">
         <marquee direction="left" scrollamount="5px" onmouseover="this.stop()" onmouseout="this.start()">
-            <a href="#">
-                <img src="{{ asset('frontend/assets/dist/img/breaking_news/american_ball_football_icon.png') }}" alt="Logo" title="Logo" width="16px">
-                Lorem ipsum dolor sit amet, consectetur.
-            </a>
-            <a href="#">
-                <img src="{{ asset('frontend/assets/dist/img/breaking_news/device_plug_icon.png') }}" alt="Logo" title="Logo" width="16px">
-                Lorem ipsum dolor sit amet.
-            </a>
-            <a href="#">
-                <img src="{{ asset('frontend/assets/dist/img/breaking_news/pine_tree_icon.png') }}" alt="Logo" title="Logo" width="16px">
-                Lorem ipsum dolor sit amet, consectetur adipisicing.
-            </a>
-            <a href="#">
-                <img src="{{ asset('frontend/assets/dist/img/breaking_news/reading_woman_icon.png') }}" alt="Logo" title="Logo" width="16px">
-                Lorem ipsum dolor sit.
-            </a>
+            @foreach($breaking_news as $item)
+                <a href="#">
+                    <img src="{{ asset('frontend/assets/dist/img/favicon.png') }}" alt="Logo" title="Logo" width="16px">
+                    {{ $item->news_title }}
+                </a>
+            @endforeach
         </marquee>
     </div>
 </div>
