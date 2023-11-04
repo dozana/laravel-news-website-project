46ced8237ef36860aa370c17b68a2f03948fb97b
Levan <levan.soroznishvili@gmail.com>
2023-11-04 15:54:20 +0400

    Output on show page category, subcategory, user image

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
-        <a class="navbar-brand" href="#">NEWSPORTAL</a>
+        <a class="navbar-brand" href="{{ url('/') }}">NEWSPORTAL</a>
 
         <ul class="navbar-nav mr-auto">
             @php
--- resources/views/frontend/news/news_details.blade.php
+++ resources/views/frontend/news/news_details.blade.php
@@ -4,8 +4,41 @@
     <div class="container flex-grow">
 
         <div class="row">
-            <div class="col-lg-12 col-md-12">
-                hi
+            <div class="col-lg-8 col-md-8">
+
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ $news['category']['category_name'] }}</li>
+                        @if($news->subcategory_id == NULL)
+                            <li class="breadcrumb-item" aria-current="page">Uncategorized</li>
+                        @else
+                            <li class="breadcrumb-item active" aria-current="page">{{ $news['subcategory']['subcategory_name'] }}</li>
+                        @endif
+                    </ol>
+                </nav>
+
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <img width="60" src="{{ (!empty($news->user->photo)) ? url('upload/admin_images/'.$news->user->photo) : url('upload/no_image.png') }}" alt="Author Image">
+                            <ul class="list-unstyled mb-0 p-2">
+                                <li>Posted By {{ $news['user']['name'] }}</li>
+                                <li><i class="far fa-clock"></i>Updated: {{ $news->created_at->format('l M d Y') }} / <i class="far fa-eye"></i> 60 Read</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="mb-3">{{ $news->news_title }}</h4>
+                        <img src="{{ (!empty($news->image)) ? asset($news->image) : url('https://via.placeholder.com/800x400') }}" class="img-fluid mb-2" alt="Post Image">
+                        {!! $news->news_details !!}
+
+                        <div>
+                            Tags: {{ $news->tags }}
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
 
