3c11d79667b6ac77ed5e0a8cac5ba926ef1e283e
Levan <levan.soroznishvili@gmail.com>
2023-11-08 00:48:50 +0400

    Change banner update markup design

--- resources/views/backend/banner/banner_update.blade.php
+++ resources/views/backend/banner/banner_update.blade.php
@@ -24,80 +24,122 @@
                             <form method="post" action="{{ route('update.banner', $banner->id) }}">
                                 @csrf
 
-                                <div class="form-group">
-                                    <img class="profile-user-img img-fluid img-square mb-2" id="show_image_1" src="{{ (!empty($banner->home_two)) ? url('upload/banner/'.$banner->home_two) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
-                                    <div class="form-group">
-                                        <label for="show_image_1">Home Banner One</label>
-                                        <div class="input-group">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_1" name="home_one">
-                                                <label class="custom-file-label" for="image_1">Choose file</label>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="show_image_1">Home Banner One</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_1" name="home_one">
+                                                    <label class="custom-file-label" for="image_1">Choose file</label>
+                                                </div>
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="show_image_1">Home Banner One</label>
+                                            <img class="profile-user-img img-fluid img-square mb-2" id="show_image_1" src="{{ (!empty($banner->home_two)) ? url('upload/banner/'.$banner->home_two) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="form-group">
-                                    <img class="profile-user-img img-fluid img-square mb-2" id="show_image_2" src="{{ (!empty($banner->home_two)) ? url('upload/banner/'.$banner->home_two) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
-                                    <div class="form-group">
-                                        <label for="show_image_2">Home Banner Two</label>
-                                        <div class="input-group">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_2" name="home_two">
-                                                <label class="custom-file-label" for="image_2">Choose file</label>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="show_image_2">Home Banner Two</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_2" name="home_two">
+                                                    <label class="custom-file-label" for="image_2">Choose file</label>
+                                                </div>
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="show_image_2">Home Banner Two</label>
+                                            <img class="profile-user-img img-fluid img-square mb-2" id="show_image_2" src="{{ (!empty($banner->home_two)) ? url('upload/banner/'.$banner->home_two) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="form-group">
-                                    <img class="profile-user-img img-fluid img-square mb-2" id="show_image_3" src="{{ (!empty($banner->home_three)) ? url('upload/banner/'.$banner->home_three) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
-                                    <div class="form-group">
-                                        <label for="show_image_3">Home Banner Three</label>
-                                        <div class="input-group">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input @error('image_3') is-invalid @enderror" id="image_3" name="home_three">
-                                                <label class="custom-file-label" for="image_3">Choose file</label>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="show_image_3">Home Banner Three</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input @error('image_3') is-invalid @enderror" id="image_3" name="home_three">
+                                                    <label class="custom-file-label" for="image_3">Choose file</label>
+                                                </div>
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="show_image_3">Home Banner Three</label>
+                                            <img class="profile-user-img img-fluid img-square mb-2" id="show_image_3" src="{{ (!empty($banner->home_three)) ? url('upload/banner/'.$banner->home_three) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="form-group">
-                                    <img class="profile-user-img img-fluid img-square mb-2" id="show_image_4" src="{{ (!empty($banner->home_four)) ? url('upload/banner/'.$banner->home_four) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
-                                    <div class="form-group">
-                                        <label for="show_image_4">Home Banner Four</label>
-                                        <div class="input-group">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_4" name="home_four">
-                                                <label class="custom-file-label" for="image_4">Choose file</label>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="show_image_4">Home Banner Four</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_4" name="home_four">
+                                                    <label class="custom-file-label" for="image_4">Choose file</label>
+                                                </div>
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="show_image_4">Home Banner Four</label>
+                                            <img class="profile-user-img img-fluid img-square mb-2" id="show_image_4" src="{{ (!empty($banner->home_four)) ? url('upload/banner/'.$banner->home_four) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="form-group">
-                                    <img class="profile-user-img img-fluid img-square mb-2" id="show_image_5" src="{{ (!empty($banner->news_category_one)) ? url('upload/banner/'.$banner->news_category_one) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
-                                    <div class="form-group">
-                                        <label for="show_image_5">News Category Banner</label>
-                                        <div class="input-group">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_4" name="news_category_one">
-                                                <label class="custom-file-label" for="image_5">Choose file</label>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="news_category_one">News Category Banner</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_5" name="news_category_one">
+                                                    <label class="custom-file-label" for="image_5">Choose file</label>
+                                                </div>
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="news_category_one">News Category Banner</label>
+                                            <img class="profile-user-img img-fluid img-square mb-2" id="news_category_one" src="{{ (!empty($banner->home_four)) ? url('upload/banner/'.$banner->home_four) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="form-group">
-                                    <img class="profile-user-img img-fluid img-square mb-2" id="show_image_6" src="{{ (!empty($banner->news_details_one)) ? url('upload/banner/'.$banner->news_details_one) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
-                                    <div class="form-group">
-                                        <label for="show_image_6">News Details Banner</label>
-                                        <div class="input-group">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_4" name="news_details_one">
-                                                <label class="custom-file-label" for="image_6">Choose file</label>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="news_details_one">News Details Banner</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input @error('image') is-invalid @enderror" id="image_6" name="news_details_one">
+                                                    <label class="custom-file-label" for="image_6">Choose file</label>
+                                                </div>
                                             </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="news_details_one">News Details Banner</label>
+                                            <img class="profile-user-img img-fluid img-square mb-2" id="news_details_one" src="{{ (!empty($banner->home_four)) ? url('upload/banner/'.$banner->home_four) : url('upload/no_image.png') }}" alt="" style="width: 400px; height: 60px;">
                                         </div>
                                     </div>
                                 </div>
@@ -144,6 +186,22 @@
                 let reader = new FileReader();
                 reader.onload = function (e) {
                     $('#show_image_4').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            });
+
+            $('#image_5').change(function (e) {
+                let reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#news_category_one').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            });
+
+            $('#image_6').change(function (e) {
+                let reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#news_details_one').attr('src', e.target.result);
                 }
                 reader.readAsDataURL(e.target.files['0']);
             });
