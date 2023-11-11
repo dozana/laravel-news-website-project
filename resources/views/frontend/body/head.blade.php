42fb8eb83f7d04ae2d03b4378f7c0f028c97777a
Levan <levan.soroznishvili@gmail.com>
2023-11-12 00:35:17 +0400

    SEO frontend output completed

--- resources/views/frontend/body/head.blade.php
+++ resources/views/frontend/body/head.blade.php
@@ -1,9 +1,14 @@
+@php
+    $seo = App\Models\Seo::find(1);
+@endphp
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<meta name="keywords" content="online newspaper, news">
-<meta name="description" content="Popular news paper of Georgia">
+<meta name="title" content="{{ $seo->meta_title }}">
+<meta name="author" content="{{ $seo->meta_author }}">
+<meta name="keywords" content="{{ $seo->meta_keywords }}">
+<meta name="description" content="{{ $seo->meta_description }}">
 <title>@yield('title')</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="{{ asset('frontend/assets/dist/css/bootstrap.css') }}">
